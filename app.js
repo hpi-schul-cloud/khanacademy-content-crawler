5eeb12a5e6214a005e68b9c60e5336c98aaffc9c
Alexander Kremer <alex@kremer.io>
2017-06-14 14:17:19 +0200

    Basic parser for topictree

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -16,7 +15,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,77 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*
+ * TOPICTREE ==> ROOT-TOPIC ==> GRADE ==> TOPIC ==> UNIT ==> CONTENT
+ * e.g. math/cc-1st-grade-math/cc-1st-geometry/cc-1st-shapes/v/sides-corners
+ * e.g. math/cc-1st-grade-math/cc-1st-geometry/cc-1st-shapes/e/attributes-of-shapes
+ * e.g. math/cc-1st-grade-math/cc-1st-add-subtract/cc-1st-add-ones-tens/a/adding-1s-and-10s-practice
+ */
+var topicTreeJson = require('../topictree.json');
+
+// TODO: Implement more content types (Exercise, Article)
+/*
+ * CONENT TYPES (How to embed):
+ * --> VIDEO (Video)
+ * --> EXERCISE (Extern Webpage)
+ * --> ARTICLE (Extern Webpage)
+ */
+let contentTypes = {
+  video: "Video"
+};
+
+/**
+ * 
+ */
 router.get('/', function(req, res, next) {
-  res.send('khanacademy crawler');
+
+  let leafNodes = getTopicTreeLeafNodes(topicTreeJson);
+  let contentNodes = leafNodes.filter(x => Object.values(contentTypes).includes(x.kind)).map(parseContent);
+
+  res.send(contentNodes);
+
 });
+
+/**
+ * 
+ * @param {*} tree 
+ */
+function getTopicTreeLeafNodes(tree) {
+  if( !tree.hasOwnProperty("children") ) {
+    return [tree];
+  }
+  return tree.children.reduce( (acc, val) => acc.concat( getTopicTreeLeafNodes(val) ), [] );
+}
+
+/**
+ * 
+ * @param {*} node 
+ */
+function parseContent(node) {
+  if(node.kind === 'Video') {
+    return parseVideo(node);
+  }
+  /*
+  if(node.kind === 'Exercise') {
+    return parseExercise(node);
+  }
+  if(node.kind === 'Article') {
+    return parseArticle(node);
+  }
+  */
+}
+
+/**
+ * 
+ * @param {*} videoNode 
+ */
+function parseVideo(videoNode) {
+  return {
+    originId: videoNode.id,
+    title: videoNode.title,
+    url: videoNode.ka_url,
+    description: videoNode.description
+  };
+}
 
 module.exports = router;
